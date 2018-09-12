8853b9efaaeed1e683075207e38dbbe43946c2e9
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-12 09:46:43 -0600

    Add Book component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class BookShelf extends Component {
     render() {
@@ -10,26 +11,7 @@ class BookShelf extends Component {
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                {this.props.books.filter(book => book.shelf === 'currentlyReading').map(book => (
-                <li>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                      <div className="book-shelf-changer">
-                        <select onChange={(e) => this.changeShelf(book,e)}>
-                          <option value="move" disabled>Move to...</option>
-                          <option value="currentlyReading">Currently Reading</option>
-                          <option value="wantToRead">Want to Read</option>
-                          <option value="read">Read</option>
-                          <option value="none">None</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors.map(author => author).filter(authors => authors.length > 1)}</div>
-                  </div>
-                </li>
-                ))}
+                {books.map(book => (<Book book={book}/>))}
               </ol>
             </div>
           </div>
