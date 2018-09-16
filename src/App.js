fa11654e76dab393f734a08341a805ebbeb8d490
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-16 09:28:41 -0600

    Add SearchBook component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './Components/BookList'
+import SearchBook from './Components/SearchBook'
+
 class BooksApp extends React.Component {
   state = {
     books: []
@@ -15,14 +16,11 @@ class BooksApp extends React.Component {
 
   fetchBooks = () => {
     BooksAPI.getAll().then((books) => {
-      console.log('data', books);
       this.setState({ books });
     });
   }
 
   updateShelf = (book, shelf) => {
-    console.log('book', book);
-    console.log('e', shelf);
     BooksAPI.update(book, shelf).then(() => {
       this.fetchBooks();
     });
@@ -35,30 +33,10 @@ class BooksApp extends React.Component {
           <BookList books={this.state.books} updateShelf={this.updateShelf}/>
         )}/>
         <Route path="/search" render={() => (
-          <div className="search-books">
-          <div className="search-books-bar">
-            <Link to="/" className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</Link>
-            <div className="search-books-input-wrapper">
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-              */}
-              <input type="text" placeholder="Search by title or author"/>
-
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
-          </div>
+          <SearchBook/>
         )}/>
       </div>
     )
   }
 }
-
 export default BooksApp
--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -20,5 +20,4 @@ class BookShelf extends Component {
     )
   }
 }
-
 export default BookShelf
