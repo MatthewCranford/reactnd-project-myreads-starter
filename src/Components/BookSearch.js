6f45f9b49ab5ea925ee9ef57480d3841d498a4f2
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-18 10:02:02 -0600

    Add searchBook function

--- src/Components/BookSearch.js
+++ src/Components/BookSearch.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
+import Book from './Book.js'
 
 
 class BookSearch extends Component {
@@ -14,8 +16,17 @@ class BookSearch extends Component {
   }
 
   changeQuery = (e) => {
+    const searchQuery = e.target.value;
     this.setState({ query: e.target.value }, () => console.log('state:', this.state));
-    console.log(this.props.books);
+    this.searchBook(searchQuery);
+  }
+
+  searchBook = (query) => {
+    console.log('query',query);
+    BooksAPI.search(query).then((books) => {
+      console.log('result', books);
+      this.setState({ books })
+    });
   }
   
   render() {
@@ -28,7 +39,11 @@ class BookSearch extends Component {
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {this.state.books.map((book, index) => (
+          <Book book={book} key={index}/>
+          ))}
+        </ol>
       </div>
       </div>
     )
