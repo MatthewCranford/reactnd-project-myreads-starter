e09e65dec5c85a9ab48e166d280db14e8bc49e39
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-23 09:40:03 -0600

    Rename ListBooks component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import BookList from './Components/BookList'
+import ListBooks from './Components/ListBooks'
 import BookSearch from './Components/BookSearch'
 
 class BooksApp extends React.Component {
@@ -36,7 +36,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <BookList books={this.state.books} updateShelf={this.updateShelf}/>
+          <ListBooks books={this.state.books} updateShelf={this.updateShelf}/>
         )}/>
         <Route path="/search" render={() => (
           <BookSearch books={this.state.books} updateShelf={this.updateShelf}/>
