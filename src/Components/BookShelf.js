ee477ef11872171565fcf5a012dad810ae95f413
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-28 10:30:48 -0600

    Convert BookShelf to functional component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BookShelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onUpdateShelf: PropTypes.func.isRequired
-  }
-
-  render() {
+function BookShelf (props) {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <h2 className="bookshelf-title">{props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.filter(book => book.shelf === this.props.shelf).map((book, index) => (<Book book={book} key={index} onUpdateShelf={this.props.onUpdateShelf}/>))}
+            {props.books.filter(book => book.shelf === props.shelf).map((book, index) => (<Book book={book} key={index} onUpdateShelf={props.onUpdateShelf}/>))}
           </ol>
         </div>
       </div>
     )
-  }
+}
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  onUpdateShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf
