5dff27b271ae2ccb0492f2467eb2be0a43887a9f
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-29 11:54:35 -0600

    Refactor Bookshelf

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 import Book from './Book'
 
 function BookShelf (props) {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {props.books.filter(book => book.shelf === props.shelf).map((book, index) => (<Book book={book} key={index} onUpdateShelf={props.onUpdateShelf}/>))}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.filter(book => book.shelf === props.shelf).map((book, index) => (<Book book={book} key={index} onUpdateShelf={props.onUpdateShelf}/>))}
+        </ol>
       </div>
-    )
+    </div>
+  )
 }
 
 BookShelf.propTypes = {
