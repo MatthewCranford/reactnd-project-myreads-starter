ec2f9dfe4e806f0e439f34408359f1ff4d0c3495
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-29 12:02:11 -0600

    Refactor ListBooks into functional component

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -1,37 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
-class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onUpdateShelf: PropTypes.func.isRequired
-  }
-
-  state = {
-    showSearchPage: false
-  }
-
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <BookShelf books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} title="Currently Reading" shelf="currentlyReading"/>
-            <BookShelf books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} title="Want to Read" shelf="wantToRead"/>
-            <BookShelf books={this.props.books} onUpdateShelf={this.props.onUpdateShelf} title="Read" shelf="read"/>
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search" onClick={() => this.setState({ showSearchPage: true })}>Add a book</Link>
+function ListBooks (props) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <BookShelf books={props.books} onUpdateShelf={props.onUpdateShelf} title="Currently Reading" shelf="currentlyReading"/>
+          <BookShelf books={props.books} onUpdateShelf={props.onUpdateShelf} title="Want to Read" shelf="wantToRead"/>
+          <BookShelf books={props.books} onUpdateShelf={props.onUpdateShelf} title="Read" shelf="read"/>
         </div>
       </div>
-    )
-  }
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
+}
+
+ListBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  onUpdateShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks
