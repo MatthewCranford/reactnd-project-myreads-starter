b38c39e58e4c391598deb617d825c80629c125ea
Matthew Cranford <mcranfordmail@gmail.com>
2018-09-29 11:55:22 -0600

    Refactor Book component into functionial component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Book extends Component {
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onUpdateShelf: PropTypes.func.isRequired
-  }
+function Book (props) {
+  return (
+    <li>
+      <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
+        <div className="book-shelf-changer">
+        <select onChange={(e) => props.onUpdateShelf(props.book, e.target.value)} value={props.book.shelf}>
+          <option value="move" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+        </div>
+      </div>
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authors && props.book.authors.map(author => author).filter(authors => authors.length > 1)}</div>
+      </div>
+    </li>
+  )
+}
 
-  render() {
-    return (
-      <li>
-        <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
-          <div className="book-shelf-changer">
-          <select onChange={(e) => this.props.onUpdateShelf(this.props.book, e.target.value)} value={this.props.book.shelf}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
-          </div>
-        </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors && this.props.book.authors.map(author => author).filter(authors => authors.length > 1)}</div>
-        </div>
-      </li>
-    )
-  }
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  onUpdateShelf: PropTypes.func.isRequired
 }
 
 export default Book
